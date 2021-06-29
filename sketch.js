aad3a07fc4699319ed54ba06f73952dc76265cd1
NIHIL NAMBIAR <74966337+nihilnambiar@users.noreply.github.com>
2021-06-29 15:48:54 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -60,21 +60,35 @@ function draw() {
   textSize(20)
  text("Score : "+score,20,30);
   Engine.update(engine);
+  textSize(35) 
+  text(" 500 ", 5, 550); text(" 500 ", 80, 550); text(" 500 ", 160, 550);
+   text(" 500 ", 240, 550); text(" 100 ", 320, 550); text(" 100 ", 400, 550); text(" 100 ", 480, 550);
+   text(" 200 ", 560, 550); text(" 200 ", 640, 550); text(" 200 ", 720, 550);
  
   if(ball!=null){
     ball.display()
-  }
-  if(particle!=null){
-    particle.display();
-    if(particle.body.position.y>760){
-      if(particle.body.position.x<300){
+  console.log(ball);
+  
+    if(ball.body.position.y>760){
+      if(ball.body.position.x<300){
         score=score+500
-        particle=null
-      
+        ball=null
+        if ( count>= 5) gameState ="end";
+      }
+      else if(ball.body.position.x<=600 && ball.body.position.x>301){
+        score=score+100
+        ball=null
+        if ( count>= 5) gameState ="end";
+      }
+      else if(ball.body.position.x<900 &&ball.body.position.x>601 ){
+        score=score+200
+        ball=null
+        if ( count>= 5) gameState ="end";
+      }
     }
     }
-  }
-  if(count>=5)gameState="end"
+  
+  //if(count>=5)gameState="end"
    for (var i = 0; i < plinkos.length; i++) {
      
      plinkos[i].display();
@@ -88,9 +102,9 @@ function draw() {
    }
 }
 function mousePressed(){
-  if(gameState!="END"){
+  if(gameState!="end"){
     count=count+1
-    ball=new Particle(mouseX,10,10,10,10)
+    ball=new Particle(mouseX,10,10)
   }
   
 }
